fdd61bc2d004808b70b1b3516ebe77d7fe81a560
Jasef HA <75513613@certus.edu.pe>
2023-12-16 04:37:23 -0500

    cors

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ from sqlalchemy.orm import sessionmaker
 from models.models import Base  # Ruta correcta para importar Base desde models
 from routes.routes import router  # Ruta correcta para importar el enrutador desde routes
 from database import get_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configuración CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Agrega la URL de tu aplicación React
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Agrega las rutas al enrutador principal
 app.include_router(router)
